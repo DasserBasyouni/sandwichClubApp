90da993ec04a104b2b52cfab21786a9713067326
Dasser Basyouni <daser4ever96@gmail.com>
2018-03-08 08:55:09 +0200

    Update DetailActivity.java

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -11,6 +11,8 @@ import com.squareup.picasso.Picasso;
 import com.udacity.sandwichclub.model.Sandwich;
 import com.udacity.sandwichclub.utils.JsonUtils;
 
+import java.util.List;
+
 public class DetailActivity extends AppCompatActivity {
 
     public static final String EXTRA_POSITION = "extra_position";
@@ -68,7 +70,18 @@ public class DetailActivity extends AppCompatActivity {
     private void populateUI(Sandwich sandwich) {
         origin_tv.setText(sandwich.getPlaceOfOrigin());
         description_tv.setText(sandwich.getDescription());
-        ingredients_tv.setText(sandwich.getIngredients().toString());
-        also_known_tv.setText(sandwich.getAlsoKnownAs().toString());
+        ingredients_tv.setText(getSensibleText(sandwich.getIngredients()));
+        also_known_tv.setText(getSensibleText(sandwich.getAlsoKnownAs()));
+    }
+
+    private String getSensibleText(List<String> ingredients) {
+        StringBuilder sb = new StringBuilder();
+        for (String s : ingredients)
+        {
+            sb.append(s);
+            sb.append(", ");
+        }
+        sb.setLength(sb.length()-2);
+        return sb.toString();
     }
 }
