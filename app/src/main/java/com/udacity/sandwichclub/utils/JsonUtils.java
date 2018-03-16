8da2740df26fa7185209d44dc02aae0624f5b326
Dasser Basyouni <daser4ever96@gmail.com>
2018-03-16 08:19:10 +0200

    small fixes
    - updated gradle for 4.1 to 4.6
    - removed the log of JSON string

--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -1,20 +1,16 @@
 package com.udacity.sandwichclub.utils;
 
-import android.util.Log;
-
 import com.udacity.sandwichclub.model.Sandwich;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class JsonUtils {
 
     public static Sandwich parseSandwichJson(String json) {
-        Log.wtf("Z_json", json);
         try {
             JSONObject jsonObject = new JSONObject(json);
             JSONObject name = jsonObject.optJSONObject("name");
